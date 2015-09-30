c75599d2e4723d30a1afaf04306de6d90ba02961
Sirs0ri <der.sas0ri@gmail.com>
2015-09-30 23:09:50 +0200

    .

--- download_test.py
+++ download_test.py
@@ -2,19 +2,29 @@
 
 import requests, re
 
-url = "http://www.couchtuner.la/tv-list/"
+listURL = "http://www.couchtuner.la/tv-list/"
 
-shows=["The 100","The Americans","Arrow","Black Mirror","The Blacklist","Blindspot","The Flash","Grimm","Hawaii Five-0","Humans","iZombie","Limitless","Agent Carter","Agents of Shield","Daredevil","Jessica Jones","Minority Report","Mr. Robot","NCIS","Numb3rs","Orphan Black","Person of Interest","Scandal","Sherlock"]
+shows=["The 100","The Americans","Arrow","Black Mirror","The Blacklist","Blindspot","The Flash","Grimm","Hawaii Five-0","Humans","iZombie","Limitless","Agent Carter","Marvels Agents of Shield","Daredevil","Jessica Jones","Minority Report","Mr. Robot","NCIS","Numb3rs","Orphan Black","Person of Interest","Scandal","Sherlock"]
 
-r = requests.get(url)
-text = r.text
+rList = requests.get(listURL)
+textList = rList.text
 
 for show in shows:
-    m = re.search(r"<li>((<a (title=\".+\" )?href=\"(?P<link1>.+)\">)(<strong>)|(<strong>)(<a (title=\".+\" )?href=\"(?P<link2>.+)\">))(?P<name>{name})(<[\s\S]*?)<\/li>".format(name=show), text)
+    regex = r"<li>((<a (title=\".+\" )?href=\"(?P<link1>.+)\">)(<strong>)|(<strong>)(<a (title=\".+\" )?href=\"(?P<link2>.+)\">))(.*?)" + r"(.*?)".join(show) + r"(.*?)(<[\s\S]*?)<\/li>"
+    
+    #Check, if the regex is part of the whole source code
+    m = re.search(regex, textList, re.IGNORECASE)
     if m:
-        name = m.group("name")
         if m.group("link1"):
-            link = m.group("link1")
+            showURL = m.group("link1")
         elif m.group("link2"):
-            link = m.group("link2")
-        print("{0}: {1}".format(name,link))
+            showURL = m.group("link2")
+        else:
+            showURL = 0
+    else: 
+        showURL = 0
+    
+    if showURL:
+        rShow = requests.get(showURL)
+        textShow = rShow.text
+        
