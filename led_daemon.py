330365eecb5f36b741326c49403c88828ebe7bfb
Sirs0ri <ertl.maxi@gmail.com>
2015-10-10 16:51:57 +0200

    Included __init__ in LightDaemon

--- led_daemon.py
+++ led_daemon.py
@@ -11,6 +11,13 @@ class LightDaemon(Daemon):
     """
     
     """
+    
+	def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+		self.stdin = stdin
+		self.stdout = stdout
+		self.stderr = stderr
+		self.pidfile = pidfile
+        
     def start(self):
         """
         Start the daemon
