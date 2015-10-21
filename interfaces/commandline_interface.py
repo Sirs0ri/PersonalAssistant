a878fae2527a4dfdae87c3c0c53f18a431dba6fb
Sirs0ri <ertl.maxi@gmail.com>
2015-10-21 03:32:22 +0200

    removed references to easygui, not importing "core" anymore

--- interfaces/commandline_interface.py
+++ interfaces/commandline_interface.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import core
+import sys, urllib
+#import core
 
 is_sam_interface = 1
 name = "Commandline Interface"
 
 def log(content):
     print("[info] " + content)
+
+def get_answer(command, keyword=None, parameter=None):
+    command = urllib.urlencode({"command":command})
+    keyword = urllib.urlencode({"keyword":keyword})
+    parameter = urllib.urlencode({"parameter":parameter})
+    answer = urllib.urlopen("http://127.0.0.1:5000?%s&%s&%s" % (command, keyword, parameter)).read()
+    if answer:
+        return answer
+    else:
+        return "Error"
 
 def initialize():
     print("Hello!")
@@ -18,4 +28,7 @@ def initialize():
         if "exit" == command:
             sys.exit()
         else:
-            print(core.get_answer(command))
+            print(get_answer(command))
+
+if __name__ == '__main__':
+    initialize()
