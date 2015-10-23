ab90bd5a6ba5e11c2c00fd22aa665676673710e4
Sirs0ri <ertl.maxi@gmail.com>
2015-10-23 15:35:58 +0200

    Added Schedule-interface that triggers every 5 m

---
+++ interfaces/schedule_interface.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, time, easygui
+import core
+from daemon import Daemon
+
+is_sam_interface = 1
+name = "Example"
+
+class Interface(Daemon):
+
+    def log(content):
+        pass
+
+    def run():
+        i = 0
+        while True:
+            time.sleep(300)
+            i += 5
+            core.get_answer("schedule", "schedule {}".format(i % 60), i)
