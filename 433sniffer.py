813b923cea8ad6f1de12ab98a787d08004b9d5d2
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-05 15:07:17 +0100

    first sketch of a python 433-sniffer

---
+++ 433sniffer.py
@@ -0,0 +1,22 @@
+# External module imports
+import RPi.GPIO as GPIO
+
+# Pin Definitons:
+
+butPin = 17 # Broadcom pin 17 (P1 pin 11)
+
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+
+print("Here we go! Press CTRL+C to exit")
+try:
+    while 1:
+        if GPIO.input(butPin): # button is released
+            print("in 1")
+            print(GPIO.input(butPin))
+        else: # button is pressed:
+            print("in 2")
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    exit()
