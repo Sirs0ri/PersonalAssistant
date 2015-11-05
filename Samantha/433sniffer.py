46ea52b6bdec3725c63d36f3c37681c96676843b
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-05 16:46:59 +0100

    switched to pigpio

--- Samantha/433sniffer.py
+++ Samantha/433sniffer.py
@@ -2,27 +2,19 @@
 # -*- coding: utf-8 -*-
 
 # External module imports
-import RPi.GPIO as GPIO
+import pigpio
 import time
 
 # Pin Definitons:
-
+pi = pigpio.pi()
 butPin = 17 # Broadcom pin 17 (P1 pin 11)
 
 
-# Pin Setup:
-GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
 print("Here we go! Press CTRL+C to exit")
 try:
-    while 1:
-        if GPIO.input(butPin): # button is released
-            print("in 1")
-            print(GPIO.input(butPin))
-            time.sleep(0.5)
-        else: # button is pressed:
-            print("in 2")
-            time.sleep(0.5)
+    for i in range(20):
+        pi.read(17)
+        time.sleep(0.5)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     exit()
