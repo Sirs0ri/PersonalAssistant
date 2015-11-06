8b981821cdf83448dca065ff2ac3f12134b24c9b
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-06 01:40:30 +0100

    . => ........

--- Samantha/_433.py
+++ Samantha/_433.py
@@ -18,7 +18,7 @@ class rx():
    wireless fobs.
    """
    def __init__(self, pi, gpio, callback=None,
-                      min_bits=1, max_bits=4096, glitch=0):
+                      min_bits=8, max_bits=4096, glitch=0):
       """
       Instantiate with the Pi and the GPIO connected to the wireless
       receiver.
