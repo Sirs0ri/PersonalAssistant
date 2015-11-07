e8d0cbcf4e93bb08ce2274358a8c4852e502a997
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-07 19:07:57 +0100

    init schedule_plugin

---
+++ Samantha/plugins/schedule_plugin.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import core, threading, time
+
+class Plugin_Thread(threading.Thread):
+    
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name + "_Thread"
+        self.running = 1
+        
+    def run(self):
+        core.log(self.name, "Started")
+        i = 0
+        while self.running:
+            core.get_answer("schedule", i, "schedule {}".format(i % 60))
+            i += 5
+            time.sleep(300)
+        core.log(self.name, "Not running anymore.")
+        
+    def stop(self):
+        self.running = 0
+        core.log(self.name, "Exited")
+        
+t = Plugin_Thread(name)
+
+def initialize():
+    global t
+    core.log(name, "Starting")
+    t.start()
+
+def stop():
+    global t
+    core.log(name, "Exiting")
+    t.stop()
