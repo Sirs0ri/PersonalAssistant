fab10a8549845127888ebf3a7268f6218f39d824
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-09 21:01:50 +0100

    please work ó.ò

--- Samantha/tools/url.cpp
+++ Samantha/tools/url.cpp
@@ -1,17 +1,8 @@
-// Edit : rewritten for cURLpp 0.7.3
-// Note : namespace changed, was cURLpp in 0.7.2 ...
+#include <CkHttp.h>
+#include <CkString.h>
 
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Options.hpp>
+CkHttp http;
+CkString html;
+html = http.quickGetStr("http://www.w3.org/Protocols/");
 
-// RAII cleanup
-
-curlpp::Cleanup myCleanup;
-
-// Send request and get a result.
-// Here I use a shortcut to get it in a string stream ...
-
-std::ostringstream os;
-os << curlpp::options::Url(std::string("http://www.wikipedia.org"));
-
-string asAskedInQuestion = os.str();
+printf("%s\n",(const char *)html);
