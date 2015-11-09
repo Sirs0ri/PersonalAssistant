059923b7e092a4a938edc7303b8b94901c328e46
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-09 03:16:33 +0100

    error handling

--- Samantha/core.py
+++ Samantha/core.py
@@ -159,6 +159,7 @@ def get_answer(k, p=None, c=None, attempt=1):
             answer = get_answer(k, p, c, attempt)
     else:
         log(name, "aborted command {}, {}, {}".format(k, p, c))
+        answer = None
     if answer:
         return answer
     else:
