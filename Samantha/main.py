fdf8501d48854e14b1de87a28b211e01d31f10db
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-12 21:57:29 +0100

    added message for not processed commands

--- Samantha/main.py
+++ Samantha/main.py
@@ -50,9 +50,13 @@ def process():
     comm = request.args.get('comm')
     core.log(name, "Keyword {key}, Parameter {param}, Command {comm}".format(key=key,param=param,comm=comm))
     #process the command
+    processed = 0
     for p in plugins:
         if key in p.keywords:
+            processed = 1
             core.log(name, "The plugin {name} mattches the keyword.".format(p.name))
+    if not processed:
+        core.log(name, "No matching Plugin found.)
     return "Processing\nKeyword {key}\nParameter {param}\nCommand {comm}".format(key=key,param=param,comm=comm)
 
 @app.route('/shutdown/')
