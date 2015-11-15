671c5ac809aa65473a7186b4db0fc8619646b552
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-15 01:49:02 +0100

    this is now a thing

--- Samantha/plugins/identicon.py
+++ Samantha/plugins/identicon.py
@@ -7,22 +7,22 @@ Reference: https://github.com/azaghal/pydenticon/blob/master/docs/usage.rst
 Might be used to create a colored overlay over a b/w wallpaper
 """
 
-import pydenticon, core
+import pydenticon, core, time, global_variables
 
-is_sam_plugin = 0
+is_sam_plugin = 1
 name = "Identicon"
-keywords = ["identicon"]
+keywords = ["identicon", "schedule_h"]
 has_toggle = 0
 has_set = 0
 
-def main():
+def generate(data="I'm Samantha"):
     """
     generates an identicon and sends it to the G2
     possibly via an AutoRemote Plugin?
     """
     generator = pydenticon.Generator(5, 5)
-    identicon = generator.generate("john.doe@example.com", 200, 200)
-    f = open("sample.png", "wb")
+    identicon = generator.generate(data, 200, 200)
+    f = open(global_variables.folder_base + "/data/identicon.png", "wb")
     f.write(identicon)
     f.close()
     # Send image to phone
@@ -30,5 +30,17 @@ def main():
 def initialize():
     core.log(name, "Everything's fine.")
 
+def stop():
+    core.log(name, "I'm not even running anymore!")
+    
 def process(key, param, comm):
-    pass
+    if key == "identicon":
+        core.log(name, "Generating an Identicon with the data {}.".format(param))
+        if param:
+            generate(param)
+        else:
+            generate()
+    elif key == "schedule_h" and param == "0":
+        generate(time.time())
+    else:
+        
