d305730e5b8ce72393ca4d27dbf378815edea371
s337805 <maximilian.ertl1@stud-mail.uni-wuerzburg.de>
2015-11-27 20:50:57 +0100

    cleanup & comments

--- Samantha/plugins/images_plugin.py
+++ Samantha/plugins/images_plugin.py
@@ -66,16 +66,12 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
     #generate the background
 
     core.log(name, "      Creating the background")
-    """
-    First of all, the background layer is created. It contains the 
-    """
     bg_layer = Image.open(global_variables.folder_base + background_path)    #the background in color
     bg_layer = bg_layer.convert("RGBA")
     converter_color = ImageEnhance.Color(bg_layer)
     bg_layer = converter_color.enhance(0.05)
     converter_brightness = ImageEnhance.Brightness(bg_layer)
     bg_layer = converter_brightness.enhance(0.8)
-    #bg_layer.convert("L")
     if DEBUG:
         bg_layer.save(global_variables.folder_base + "/data/bg_layer.png")
 
@@ -83,8 +79,8 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
 
     core.log(name, "      Creating the mask")
     size = bg_layer.size
-    mask_BoW = Image.open(global_variables.folder_base + mask_path)    #"BoW" means black icon on white background. "WoB" is a white icon on black bg.
-    mask_WoB = ImageOps.invert(mask_BoW)
+    mask_BoW = Image.open(global_variables.folder_base + mask_path)     #"BoW" means black icon on white background.
+    mask_WoB = ImageOps.invert(mask_BoW)                                #"WoB" is a white icon on black bg.
     if not mask_BoW.size == size:
         mask_WoB = resize(mask_WoB, size)
         mask_BoW = ImageOps.invert(mask_WoB)
@@ -94,10 +90,10 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
         mask_WoB.save(global_variables.folder_base + "/data/mask_WoB.png")
         mask_BoW.save(global_variables.folder_base + "/data/mask_BoW.png")
 
-    #generate the overlay
+    #generate the colored overlay
 
-    core.log(name, "      Creating the overlay")
-    overlay_layer = Image.open(global_variables.folder_base + background_path)    #the overlay, black on white
+    core.log(name, "      Creating the colored overlay")
+    overlay_layer = Image.open(global_variables.folder_base + background_path)
     overlay_layer.putalpha(mask_WoB)
     if DEBUG:
         overlay_layer.save(global_variables.folder_base + "/data/overlay_layer.png")
@@ -108,19 +104,9 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
     shadow_layer =  mask_BoW.convert("RGBA")
     shadow_layer.putalpha(mask_WoB)
     """
-    A copy if the image is pasted ontop of it with a 2px offset into the 4 diagonal directions to increase it's size.
-    Otherwise the shadow would disappear behind the colored overlay.
+    A copy if the mask is pasted ontop of itself with a 2px offset into the 4 diagonal directions each to increase it's size into each direction. (The shadow would otherwise disappear behind the colored overlay.)
     The whole layer is then blurred multiple times to increase the blur's effect.
-    Finally, the shadow_layer is blended with a completely transparent layer to add some transparency.
-    
-    This doesn't work well though. The background behind the image becomes greyish.
-    Another attempt would be to iterate over the pixels and set the background's color to 
-        original_color *= (1 - (alpha_mask / 255))
-        
-        -> alpha_mask = 255 (= black mask)
-            (1 - (255 / 255)) = 1 - 1 = 0
-        -> alpha_mask = 0 (= no mask)
-            (1 - (0 / 255)) = 1 - 0 = 1
+    Finally, the shadow_layer is blended with a completely transparent layer to add some transparency to it.
     """
     offset_layers = []
     offsets = [(2,2),(-2,2),(2,-2),(-2,-2)]
@@ -130,16 +116,23 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
         shadow_layer.paste(offset_layer, None, offset_layer)
     core.log(name, "      Blurring the shadow")
     n = 0
-    while n < 5:    #as noted above, the Blur is applied multiple times for a stronger effect.
+    repetitions = 3     #as noted above, the Blur is applied multiple times for a stronger effect.
+    while n < repetitions:
         n += 1
-        core.log(name, "        Step {}/5".format(n))
+        core.log(name, "        Step {}/{}".format(n, repetitions))
         shadow_layer = shadow_layer.filter(ImageFilter.BLUR)
-    #core.log(name, "      Adding transparency")
-    #shadow_layer = Image.blend(Image.new("RGBA", size), shadow_layer, 0.7)
+    core.log(name, "      Adding transparency")
+    shadow_layer = Image.blend(Image.new("RGBA", size), shadow_layer, 0.7)
     if DEBUG:
         shadow_layer.save(global_variables.folder_base + "/data/shadow_layer.png")
 
     #merge the shadow with the background
+    
+    """
+    This will add the shadow manually to the background image. Usign PIL's merge() or composite_alpha() was causing the image underneath the shadow to turn greyish which looked pretty ugly on dark parts of the image.
+    To prevent this behaviour, this tool iterates over the pixels of the whole image and changes the colorvalues according to the alphavalue of the corresponding image in the mask.
+    If the pixel in the mask is completely transparent (a_mask == 0) the calculation will be skipped and the original pixel won't be changed. If the pixel in the mask is completely opaque (a_mask == 255) the pixel in the original image will be turned black. For every other alpha value the new color is calculated via new = old * (1 - (a_mask / 255)).
+    """
     core.log(name, "      Merging the shadow with the background")
     try:
         pixels = 0
@@ -148,13 +141,16 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
         for x in range(bg_layer.size[0]):
             for y in range(bg_layer.size[1]):
                 r_mask, g_mask, b_mask, a_mask = pixels_mask[x, y]
-                if a_mask:
+                if 0 < a_mask < 255:
                     r_bg, g_bg, b_bg, a_bg = pixels_bg[x, y]
                     factor = (1.0 - (float(a_mask) / 255.0))
                     r_bg = int(float(r_bg) * factor)
                     g_bg = int(float(g_bg) * factor)
                     b_bg = int(float(b_bg) * factor)
                     pixels_bg[x, y] = (r_bg, g_bg, b_bg, a_bg)
+                elif a_mask == 255:
+                    a_bg = pixels_bg[x, y][-1]
+                    pixels_bg[x, y] = (0, 0, 0, a_bg)
                     pixels += 1
     except Exception as e:
         core.log(name, "Error: {}".format(e))
@@ -171,15 +167,9 @@ def generate_wallpaper(background_path, mask_path, destination_path="/data/wallp
     
     core.log(name, "      Creating the final image")
     final = Image.new("RGBA", size)
-    #final.paste(bg_layer.convert("LA"))
     final.paste(bg_layer)
-    '''
-    final.paste(shadow_layer, None, shadow_layer)
-    '''
-    
+    #final.paste(shadow_layer, None, shadow_layer)
     final.paste(overlay_layer, None, overlay_layer)
-    #final = Image.alpha_composite(final, shadow_layer)
-    #final = Image.alpha_composite(final, overlay_layer)
     final.save(global_variables.folder_base + destination_path)
     core.log(name, "    Created the wallpaper at {}.".format(global_variables.folder_base + destination_path))
     return destination_path
