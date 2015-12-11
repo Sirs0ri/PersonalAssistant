8a5e6f35fbb0cd8d09b48cc0b87bbd57b53946fe
Sirs0ri <ertl.maxi@gmail.com>
2015-12-11 20:07:45 +0100

    removed stop() from the start-functio

--- led_daemon.py
+++ led_daemon.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         if "start" == sys.argv[1]:
             print("Starting..")
-            daemon.restart()
+            daemon.start()
             daemon.create()
         elif "stop" == sys.argv[1]:
             print("Stopping..")
