f167f056f90dded3fcfeceb44c4a949ccee32ccc
Sirs0ri <ertl.maxi@gmail.com>
2016-01-15 17:52:58 +0100

    Core: Final changes to Logging.. Hopefully

--- Samantha/core.py
+++ Samantha/core.py
@@ -74,10 +74,10 @@ def log(name="None", content=["None"], level="logging"):
     else:
         attr = []
         lvl_str="LVL"
-    
-    if len(name) < 10:
-        name += " "*(10-len(name))
-    s = "\033[90m[\033[{lvl_begin}m{lvl_str}\033[90m]  {time}  {name}:\t\033[97m{content}\033[0m".format(lvl_begin = ";".join(attr), lvl_str=lvl_str, time=time.strftime("%H:%M:%S", time.localtime()), name=name, content="\n                  \t  ".join(content))
+    name += ":"
+    if len(name) < 11:
+        name += " "*(11-len(name))
+    s = "\033[90m[\033[{lvl_begin}m{lvl_str}\033[90m]  {time}  {name}\t\033[97m{content}\033[0m".format(lvl_begin = ";".join(attr), lvl_str=lvl_str, time=time.strftime("%H:%M:%S", time.localtime()), name=name, content="\n                  \t  ".join(content))
     print(s)
     '''
     #log in file
