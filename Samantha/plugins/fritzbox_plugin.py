c94f80a99f72391ed826558bb408f54581a7fd6a
Sirs0ri <ertl.maxi@gmail.com>
2016-01-27 14:46:13 +0100

    fritzbox_plugin: Fixed a typo, added some more explanation to blacklisting devices

--- Samantha/plugins/fritzbox_plugin.py
+++ Samantha/plugins/fritzbox_plugin.py
@@ -30,7 +30,7 @@ def update_devices():
     global old_devicesdict
     global fritzhosts
     deviceslist = fritzhosts.get_hosts_info()
-    ignored_macs = ["00:80:77:F2:71:23"]    # this list holds the mac-addresses of ignoreg devices.
+    ignored_macs = ["00:80:77:F2:71:23"]    # this list holds the mac-addresses of ignored devices. They won't be able to trigger events such as coming on/offline or registering. The 1st listed address is for example my printer which dis- and reconnects every few minutes and only spams my logs.
     devicesdict = { i["mac"]: i for i in deviceslist if i["mac"] not in ignored_macs}
     updated = 0
     new = 0
