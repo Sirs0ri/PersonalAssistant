6907713d6508891a5df6f215f21e6d5999de8cd5
Maximilian Ertl <ertl.maxi@gmail.com>
2016-04-02 22:36:21 +0200

    changed a string

--- Samantha/main.py
+++ Samantha/main.py
@@ -6,7 +6,7 @@ import core, sys, traceback
 
 """
 This is the main part, the "core" of Samantha.
-It starts a Flask-Server which is used to receive commands (via Lan or localhost) on port 5000 and initializes the plugins via 'core'.
+It starts a Flask-Server which is used to receive commands (via LAN or localhost) on port 5000 and initializes the plugins via 'core'.
 """
 
 name = "Mainframe"
@@ -47,7 +47,7 @@ def shutdown():
 @app.route('/restart/')
 def restart():
     """
-    Restart the complete program. It'll shutdown Flask and set the Restart-Flag back to 1 so that main() will be executed again after it's completed (aka after Flask and the Plugins are shut down correctly.) 
+    Restart the complete program. It'll shutdown Flask and set the Restart-Flag back to 1 so that main() will be executed again after it's completed (aka after Flask and the Plugins are shut down correctly.)
     """
     global restart
     core.log(name, ["Received the request to restart."], "warning")
@@ -58,24 +58,24 @@ def restart():
 
 def main():
     """
-    This is the main function. 
+    This is the main function.
     It starts everything and does stuff.
     """
     global app
     global restart
     core.log(name, [""," ____    _    __  __    _    _   _ _____ _   _    _","/ ___|  / \  |  \/  |  / \  | \ | |_   _| | | |  / \ ","\___ \ / _ \ | |\/| | / _ \ |  \| | | | | |_| | / _ \ "," ___) / ___ \| |  | |/ ___ \| |\  | | | |  _  |/ ___ \ ","|____/_/   \_\_|  |_/_/   \_\_| \_| |_| |_| |_/_/   \_\ ","                                                    hi~", "Starting up!"], "info")
-    
+
     restart = 0
     startup = core.startup()
     '''
     if not startup:
-        #initialisation went wrong, abort 
+        #initialisation went wrong, abort
     '''
-        
+
     #don't log "INFO"-messages from Flask/werkzeug
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.WARNING)
-    
+
     #app.debug = True
     core.log(name, ["Starting Flask..."], "info")
     try:
@@ -87,10 +87,10 @@ def main():
         traceback.print_exc(file=sys.stdout)
         print("-"*60)
         core.log(name, ["{}".format(e)], "error")
-    
+
     core.process(key="onexit", origin=name, target="all")
     core.log(name, ["  Plugins stopped."], "logging")
-    
+
     #this'll be executed when Flask stops.
     core.log(name, ["Shut down successfully."], "info")
 
