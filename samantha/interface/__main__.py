742ba0e2b9b41ba9172d7e24b0e8487ca3eb3d6a
Maximilian Ertl <ertl.maxi@gmail.com>
2016-04-08 22:54:41 +0200

    Implemented Controller for interfaces. started with python interface.

---
+++ samantha/interface/__main__.py
@@ -0,0 +1,7 @@
+"""Starts the main interface. To be called with 'python interface'
+from the root folder. After importing it, the interface will be
+started start"""
+
+if __name__ == '__main__':
+    import simple_cli as the_interface
+    the_interface.start()
