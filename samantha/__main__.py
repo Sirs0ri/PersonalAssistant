91dabdd8aed4a81150cd813c14712bfaf7d0756f
Maximilian Ertl <ertl.maxi@gmail.com>
2016-04-14 22:05:49 +0200

    renamed "log" to "logger"

--- samantha/__main__.py
+++ samantha/__main__.py
@@ -1,12 +1,12 @@
 """Samantha's main module. calling this via 'python samantha'
 should start everything else"""
 import logging
-import log
+import logger
 
 LOGGER = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    log.configure_logging()
+    logger.configure_logging()
     LOGGER.debug("-"*47)
     LOGGER.debug("Starting Samantha")
     LOGGER.debug("-"*47)
