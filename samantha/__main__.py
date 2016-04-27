f490c5f7a745f271240c82daab7ef25f33b65bf2
Maximilian Ertl <ertl.maxi@gmail.com>
2016-04-27 02:40:02 +0200

    removed manual initialisation - now auto-initializing

--- samantha/__main__.py
+++ samantha/__main__.py
@@ -7,7 +7,6 @@ import logger
 LOGGER = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    logger.configure_logging()
     LOGGER.debug("-"*47)
     LOGGER.debug("Starting Samantha")
     LOGGER.debug("-"*47)
