c06acb337098762dd8c31737ec9806df1189fe91
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-07 14:00:18 +0200

    :new: Added the server

--- samantha/__main__.py
+++ samantha/__main__.py
@@ -2,6 +2,10 @@
 should start everything else"""
 import logging
 
+from autobahn.twisted.websocket import WebSocketServerProtocol, \
+                                       WebSocketServerFactory
+from twisted.internet import reactor
+
 import logger
 
 # TODO import only what's necessary
@@ -14,7 +18,34 @@ import tools
 logger.initialize()
 LOGGER = logging.getLogger(__name__)
 
+
+class Server(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+        LOGGER.info("Client connecting: %s", request.peer)
+
+    def onOpen(self):
+        LOGGER.info("WebSocket connection open.")
+        # self.factory.reactor.callLater(5, reactor.stop)
+
+    def onClose(self, wasClean, code, reason):
+        LOGGER.info("WebSocket connection closed: %s", reason)
+        reactor.stop()
+
+    def onMessage(self, payload, isBinary):
+        # echo back message verbatim
+        if isBinary:
+            LOGGER.debug("Binary message received: %d bytes", len(payload))
+        else:
+            LOGGER.debug("Text message received: %s", payload.decode('utf8'))
+            if payload.decode('utf8') == "exit_server":
+                LOGGER.fatal("Received the request to close the server")
+                self.sendClose()
+        self.sendMessage(payload, isBinary)
+
+
 if __name__ == "__main__":
+    LOGGER.info("Initializing...")
     LOGGER.debug("-"*47)
     LOGGER.debug("Starting Samantha")
     LOGGER.debug("-"*47)
@@ -24,3 +55,8 @@ if __name__ == "__main__":
     # Start the core
     # Initialize devices and services
     # Start updater as part of main
+    factory = WebSocketServerFactory()
+    factory.protocol = Server
+
+    port = reactor.listenTCP(9000, factory)
+    reactor.run()
