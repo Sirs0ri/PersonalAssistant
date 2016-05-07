d201fe813c17287e140994359c46589fdb556a96
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-07 13:58:07 +0200

    :tada: cleaned up imports

--- samantha/tools/__init__.py
+++ samantha/tools/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+import queues
 
 LOGGER = logging.getLogger(__name__)
 INITIALIZED = False
@@ -9,7 +10,6 @@ LOGGER.debug("I was imported.")
 
 def init():
     LOGGER.info("Initializing...")
-    import queues
     return True
 
 if not INITIALIZED:
