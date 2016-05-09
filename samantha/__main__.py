1d06f83d09c265034bfa0fb929b1900061dbc3c3
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-09 23:32:29 +0200

    :memo:/:tada: code cleanup + updated TODO (8/8)
    * TODO of each file is now bundled in the beginning
    * added DocStrings/comments
    * refined logging a bit
    * reordered code for better readability (mostly by using newlines)

--- samantha/__main__.py
+++ samantha/__main__.py
@@ -1,53 +1,72 @@
 """Samantha's main module. calling this via 'python samantha'
 should start everything else"""
 import logging
+import Queue
 
 from autobahn.twisted.websocket import WebSocketServerProtocol, \
                                        WebSocketServerFactory
-import Queue
 from twisted.internet import reactor
-
-import logger
 
 # TODO import only what's necessary
 import context
 import core
 import devices
+import logger
 import services
 import tools
+
 
 logger.initialize()
 LOGGER = logging.getLogger(__name__)
 
+UID = 0
+
+def getUID():
+    global UID
+    UID += 1
+    return UID
 
 class Server(WebSocketServerProtocol):
 
+    C_UID = None
+
     def onConnect(self, request):
-        LOGGER.info("Client connecting: %s", request.peer)
+        self.C_UID = getUID()
+        LOGGER.info("[C_UID: %d] Client connecting: '%s'",
+                    self.C_UID,
+                    request.peer)
 
     def onOpen(self):
-        LOGGER.info("WebSocket connection open.")
+        LOGGER.info("[C_UID: %d] WebSocket connection open.",
+                    self.C_UID)
         # self.factory.reactor.callLater(5, reactor.stop)
 
     def onClose(self, wasClean, code, reason):
-        LOGGER.info("WebSocket connection closed: %s", reason)
+        LOGGER.info("[C_UID: %d] WebSocket connection closed: '%s'",
+                    self.C_UID,
+                    reason)
         # reactor.stop()
 
     def onMessage(self, payload, isBinary):
         # echo back message verbatim
         if isBinary:
-            LOGGER.debug("Binary message received: %d bytes", len(payload))
+            LOGGER.debug("[C_UID: %d] Binary message received: %d bytes",
+                         self.C_UID,
+                         len(payload))
         else:
-            LOGGER.debug("Text message received: %s", payload.decode('utf8'))
+            LOGGER.debug("[C_UID: %d] Text message received: '%s'",
+                         self.C_UID,
+                         payload.decode('utf8'))
             if payload.decode('utf8') == "exit_server":
-                LOGGER.fatal("Received the request to close the server")
+                LOGGER.fatal(
+                    "[C_UID: %d] Received the request to close the server",
+                    self.C_UID)
                 self.sendClose()
                 reactor.stop()
             else:
                 INPUT.put({"self": self,
                            "payload": payload.decode('utf8'),
                            "isBinary": isBinary})
-        # self.sendMessage(payload, isBinary)
 
 
 if __name__ == "__main__":
