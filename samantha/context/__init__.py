c41d6e35b9e3f655c326af9b3c2f8688440cb8c2
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-09 23:28:07 +0200

    :memo:/:tada: code cleanup + updated TODO (1/8)
    * TODO of each file is now bundled in the beginning
    * added DocStrings/comments
    * refined logging a bit
    * reordered code for better readability (mostly by using newlines)

--- samantha/context/__init__.py
+++ samantha/context/__init__.py
@@ -1,41 +1,61 @@
+"""Samantha's context module.
+
+ - Handles storing the current context
+ - saves/loads it when the program is ended/started.
+ - Repeatedly compares the context against a given set of rules to allow basic
+   automation"""
+
+###############################################################################
+#
+# TODO: [ ] finish initialize() and stop()
+# TODO: [ ]     Start a new context, then load the last one
+# TODO: [ ]     Start context-updater
+# TODO: [ ]         Checks if properties are expired
+# TODO: [ ]         Compares Context and rules
+# TODO: [ ] finish stop()
+# TODO: [ ]     Store the current context as JSON
+# TODO: [ ] def import_from_file(path="data/context.json"):
+# TODO: [ ]     Load a previous context from JSON, then:
+# TODO: [ ]     Compare the property's TTLs to the current time and load only
+#               valide ones
+# TODO: [ ] def setProperty(property, value, ttl):
+# TODO: [ ] def getProperty(property):
+# TODO: [ ] def addRule(rule):
+#
+###############################################################################
+
+
 import logging
 
+
+# Initialize the logger
 LOGGER = logging.getLogger(__name__)
+
+# Set constants
 INITIALIZED = False
 
 LOGGER.debug("I was imported.")
 
 
 def _init():
+    """Initializes the module."""
     LOGGER.info("Initializing...")
-    # TODO Start the context, maybe even load the last one
-    # TODO Start updater
-    #      Checks if properties are expired
-    #      Compares Context and rules
+    LOGGER.info("Initialisation complete.")
     return True
 
 
 def stop():
+    """Stops the module."""
     global INITIALIZED
+
     LOGGER.info("Exiting...")
-    # TODO Dump the context to data/context.DATE.json
     INITIALIZED = False
+    LOGGER.info("Exited.")
     return True
-
-# TODO def import_from_file(path="data/context.json"):
-    # TODO Load the file as json
-    # TODO Compare the property's TTLs to the current time
-    #      and load only valide ones
-
-
-# TODO def setProperty(property, value, ttl):
-
-# TODO def getProperty(property):
-
-# TODO def addRule(rule):
 
 
 def initialize():
+    """Initialize the module when not yet initialized."""
     global INITIALIZED
     if not INITIALIZED:
         INITIALIZED = _init()
