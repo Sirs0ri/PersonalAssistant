02ed5e0b8d52e78b47aac1e8b7b849a3721882d8
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-09 23:28:48 +0200

    :memo:/:tada: code cleanup + updated TODO (3/8)
    * TODO of each file is now bundled in the beginning
    * added DocStrings/comments
    * refined logging a bit
    * reordered code for better readability (mostly by using newlines)

--- samantha/devices/__init__.py
+++ samantha/devices/__init__.py
@@ -1,30 +1,56 @@
+"""Samantha's devices module.
+
+ - forwards commands to devices (like set property COLOR of LED to GREEN)
+ - fires events into the INPUT queue when an device's status changes
+   (e.g. if it becomes un-/available)"""
+
+###############################################################################
+#
+# TODO: [ ] _init()
+# TODO: [ ]     load devices
+# TODO: [ ] stop()
+# TODO: [ ]     stop devices if necessary
+# TODO: [ ] def process(keyword, params={}):
+# TODO: [ ] monitor status changes
+#
+###############################################################################
+
+
 import logging
 
+
+# Initialize the logger
 LOGGER = logging.getLogger(__name__)
+
+# Set constants
 INITIALIZED = False
 
 LOGGER.debug("I was imported.")
 
 
 def _init():
+    """Initializes the module."""
     LOGGER.info("Initializing...")
-    # TODO Load all devices
+
+    # initialize all devices
+    LOGGER.info("Initialisation complete.")
     return True
 
 
 def stop():
+    """Stops the module."""
     global INITIALIZED
+
     LOGGER.info("Exiting...")
-    # TODO stop devices
     INITIALIZED = False
+
+    # Stop all devices
+    LOGGER.info("Exited.")
     return True
 
 
-# TODO def load_devices():
-
-# TODO def process(keyword, params={})
-
 def initialize():
+    """Initialize the module when not yet initialized."""
     global INITIALIZED
     if not INITIALIZED:
         INITIALIZED = _init()
