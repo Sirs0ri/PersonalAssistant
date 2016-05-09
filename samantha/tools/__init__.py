b28881c74135301156c7ab47e1c9df7d051ea199
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-09 23:32:01 +0200

    :memo:/:tada: code cleanup + updated TODO (7/8)
    * TODO of each file is now bundled in the beginning
    * added DocStrings/comments
    * refined logging a bit
    * reordered code for better readability (mostly by using newlines)

--- samantha/tools/__init__.py
+++ samantha/tools/__init__.py
@@ -1,26 +1,56 @@
+"""A collection of different tools Samantha might use.
+
+ - Updater, to monitor it's sources on GitHub and automatically update to newer
+   versions, if available and if a certain series of tests passes"""
+
+###############################################################################
+#
+# TODO: [ ] Updater
+# TODO: [ ]     Monitor Sources for the modules
+# TODO: [ ]     Test new versions
+# TODO: [ ]     replace them on-the-go if tests are passed
+# TODO: [ ]     keep the old version as backup for a certain time (maybe check
+#               every 24h and discard old versions 24h later if nothing's gone
+#               wrong?)
+#
+###############################################################################
+
+
 import logging
 
-# TODO: Add Updater
 
+# Initialize the logger
 LOGGER = logging.getLogger(__name__)
+
+# Set constants
 INITIALIZED = False
 
 LOGGER.debug("I was imported.")
 
 
 def _init():
+    """Initializes the module."""
     LOGGER.info("Initializing...")
+
+    # initialize all tools
+    LOGGER.info("Initialisation complete.")
     return True
 
 
 def stop():
+    """Stops the module."""
     global INITIALIZED
+
     LOGGER.info("Exiting...")
     INITIALIZED = False
+
+    # Stop all tools
+    LOGGER.info("Exited.")
     return True
 
 
 def initialize():
+    """Initialize the module when not yet initialized."""
     global INITIALIZED
     if not INITIALIZED:
         INITIALIZED = _init()
