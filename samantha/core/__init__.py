c156fa0a9769945db57084f7e19fe21ff35633b9
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-24 19:46:35 +0200

    :memo: updated TODO

--- samantha/core/__init__.py
+++ samantha/core/__init__.py
@@ -16,11 +16,11 @@
 # TODO: [ ]     parse the message
 # TODO: [ ]     forward it to services and/or devices
 # TODO: [ ] sender()
-# TODO: [ ]     move to a new server-module
-# TODO: [ ]     check if the message cam from an external source, don't do
-#               anything if not (e.g. if the original trigger came via plugin)
+# TODO: [ ]     check where the message came from an external source, send the
+#               result to the correct server/device/service
+# TODO: [ ] move Server into a standalone module
 # TODO: [ ] _init()
-# TODO: [ ]     move senderto a new server-module
+# TODO: [ ]     move sender to a new server-module
 #
 ###############################################################################
 
