ae938fa60cbf9535a90e66fb6a75e348ac85b1fd
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-24 20:52:20 +0200

    stop the core as 1st module

--- samantha/__main__.py
+++ samantha/__main__.py
@@ -47,8 +47,8 @@ if __name__ == "__main__":
                              keyword="onexit").trigger()
 
     LOGGER.info("Exiting...")
+    core.stop()
     context.stop()
     devices.stop()
     services.stop()
     tools.stop()
-    core.stop()
