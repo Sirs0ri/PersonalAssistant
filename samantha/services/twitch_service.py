52b0c722bbab022a8509994c582a2b6923fbb068
Maximilian Ertl <ertl.maxi@gmail.com>
2016-07-06 00:35:13 +0200

    :tada: code cleanup

--- samantha/services/twitch_service.py
+++ samantha/services/twitch_service.py
@@ -21,37 +21,37 @@ from services.service import BaseClass
 from tools import eventbuilder
 try:
     import variables_private
-    secrets = {
+    SECRETS = {
         "oauth_token": variables_private.twitch_oauth_token,
         "client_id": variables_private.twitch_client_id}
 except (ImportError, AttributeError):
     variables_private = None
-    secrets = None
+    SECRETS = None
 # pylint: enable=import-error
 
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 # Initialize the logger
 LOGGER = logging.getLogger(__name__)
 
 if variables_private is None:
     LOGGER.exception("Couldn't access the private variables.")
-if secrets is None:
+if SECRETS is None:
     LOGGER.exception("Couldn't access the API-Key and/or client-ID.")
 
-SERVICE = BaseClass("Twitch", secrets is not None, LOGGER, __file__)
+SERVICE = BaseClass("Twitch", SECRETS is not None, LOGGER, __file__)
 
-streamlist = []
+STREAM_LIST = []
 
 
 @subscribe_to(["onstart", "schedule_min"])
 def check_followed_streams(key, data):
     """Check for new online streams on twitch.tv."""
-    global streamlist
+    global STREAM_LIST
     # Make the http-request
     url = "https://api.twitch.tv/kraken/streams/followed"
-    req = requests.get(url, params=secrets)
+    req = requests.get(url, params=SECRETS)
     data = json.loads(req.text)
     new_streamlist = {}
     # parse the data
@@ -61,7 +61,7 @@ def check_followed_streams(key, data):
         for item in data:
             channelname = item["channel"]["name"]
             new_streamlist[channelname] = item
-            if channelname not in streamlist:
+            if channelname not in STREAM_LIST:
                 # That means the stream came online since the last check
                 LOGGER.debug("'%s' is now online.", channelname)
                 eventbuilder.Event(
@@ -72,16 +72,16 @@ def check_followed_streams(key, data):
             else:
                 LOGGER.debug("'%s' is still online.", channelname)
                 # The stream is online and already was at the last check
-                del streamlist[channelname]
+                del STREAM_LIST[channelname]
 
-    for channelname in streamlist:
-        # self.streamlist now contains all those streams that were online
+    for channelname in STREAM_LIST:
+        # STREAM_LIST now contains all those streams that were online
         # during the last check but have gone offline since.
         LOGGER.debug("'%s' is now offline.", channelname)
         eventbuilder.Event(
             sender_id=SERVICE.name,
             keyword="media.twitch.offline.{}".format(channelname),
-            data=streamlist[channelname]).trigger()
-    # update the existing streamlist with the new streams
-    streamlist = new_streamlist
+            data=STREAM_LIST[channelname]).trigger()
+    # update the existing STREAM_LIST with the new streams
+    STREAM_LIST = new_streamlist
     return True
