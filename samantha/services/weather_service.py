491e846fa4e7092a5acc210bdac347f1c8a25c9a
Maximilian Ertl <ertl.maxi@gmail.com>
2016-07-06 00:22:42 +0200

    :heavy_minus_sign: removed depreceated code

--- samantha/services/weather_service.py
+++ samantha/services/weather_service.py
@@ -30,7 +30,7 @@ except (ImportError, AttributeError):
 # pylint: enable=import-error
 
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 
 # Initialize the logger
@@ -64,49 +64,3 @@ def check_weather(key, data):
         LOGGER.exception("Exception while connecting to OWM:\n%s",
                          traceback.format_exc())
         return False
-
-
-# class Service(BaseClass):
-#     """Service that implements the Open Weather Map API."""
-#
-#     def __init__(self, uid):
-#         """Initialize the service."""
-#         LOGGER.info("Initializing...")
-#         self.name = "Weather"
-#         self.uid = uid
-#         self.keywords = ["onstart", "schedule_hour"]
-#         try:
-#             self.api_key = variables_private.owm_key
-#             self.location = variables_private.owm_location
-#             active = True
-#         except AttributeError:
-#             LOGGER.exception("Couldn't access the API-Key and/or location.")
-#             self.api_key = ""
-#             self.location = ""
-#             active = False
-#         self.weather = None
-#         super(Service, self).__init__(
-#             logger=LOGGER, file_path=__file__, active=active)
-#
-#     def process(self, key, data=None):
-#         """Process an event from the core."""
-#         if key in ["onstart", "schedule_hour"]:
-#             LOGGER.debug("Checking the Weather..")
-#             try:
-#                 req = requests.get("{baseurl}?{location}&{key}".format(
-#                     baseurl="http://api.openweathermap.org/data/2.5/weather",
-#                     location="id=" + self.location,
-#                     key="appid=" + self.api_key),
-#                                    timeout=3)
-#                 if req.status_code == 200:
-#                     eventbuilder.Event(sender_id=self.name,
-#                                        keyword="weather",
-#                                        data=req.json()).trigger()
-#                     return True
-#             except Exception:
-#                 LOGGER.exception("Exception while connecting to OWM:\n%s",
-#                                  traceback.format_exc())
-#                 return False
-#         else:
-#             LOGGER.warn("Keyword not in use. (%s, %s)", key, data)
-#         return False
