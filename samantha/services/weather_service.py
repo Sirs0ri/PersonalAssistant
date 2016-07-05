dc01e8af9ec495ba73cc8dfaf221fd724dcc210a
Maximilian Ertl <ertl.maxi@gmail.com>
2016-07-06 00:37:28 +0200

    :tada: code cleanup (docstrings + PEP8)

--- samantha/services/weather_service.py
+++ samantha/services/weather_service.py
@@ -21,16 +21,16 @@ from services.service import BaseClass
 from tools import eventbuilder
 try:
     import variables_private
-    api_key = variables_private.owm_key
-    location = variables_private.owm_location
+    API_KEY = variables_private.owm_key
+    LOCATION = variables_private.owm_location
 except (ImportError, AttributeError):
     variables_private = None
-    api_key = None
-    location = None
+    API_KEY = None
+    LOCATION = None
 # pylint: enable=import-error
 
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 
 # Initialize the logger
@@ -38,9 +38,9 @@ LOGGER = logging.getLogger(__name__)
 
 if variables_private is None:
     LOGGER.exception("Couldn't access the private variables.")
-if api_key is None:
+if API_KEY is None:
     LOGGER.exception("Couldn't access the API-Key.")
-if location is None:
+if LOCATION is None:
     LOGGER.exception("Couldn't access the Location.")
 
 SERVICE = BaseClass("Weather", True, LOGGER, __file__)
@@ -48,12 +48,13 @@ SERVICE = BaseClass("Weather", True, LOGGER, __file__)
 
 @subscribe_to(["onstart", "schedule_hour"])
 def check_weather(key, data):
+    """Check the weather."""
     LOGGER.debug("Checking the Weather..")
     try:
         req = requests.get("{baseurl}?{location}&{key}".format(
             baseurl="http://api.openweathermap.org/data/2.5/weather",
-            location="id=" + location,
-            key="appid=" + api_key),
+            location="id=" + LOCATION,
+            key="appid=" + API_KEY),
                            timeout=3)
         if req.status_code == 200:
             eventbuilder.Event(sender_id=SERVICE.name,
@@ -63,4 +64,4 @@ def check_weather(key, data):
     except Exception:
         LOGGER.exception("Exception while connecting to OWM:\n%s",
                          traceback.format_exc())
-        return False
+    return False
