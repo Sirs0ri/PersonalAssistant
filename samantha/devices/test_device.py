6e5194145b9478740a30b1f848a0b3bbea2db133
Maximilian Ertl <ertl.maxi@gmail.com>
2016-07-06 00:28:46 +0200

    :heavy_plus_sign: added dockstrings

--- samantha/devices/test_device.py
+++ samantha/devices/test_device.py
@@ -19,7 +19,7 @@ from devices.device import BaseClass
 # pylint: enable=import-error
 
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 
 # Initialize the logger
@@ -30,23 +30,27 @@ DEVICE = BaseClass("Test", True, LOGGER, __file__)
 
 @subscribe_to("onstart")
 def start_func(key, data):
+    """Test the 'onstart' event."""
     LOGGER.debug("I'm now doing something productive!")
     return True
 
 
 @subscribe_to(["test", "test.1", "test.device"])
 def test1(key, data):
+    """Test various events."""
     LOGGER.warn("Test1 successful!\n%s - %s", key, data)
     return True
 
 
 @subscribe_to("test.2")
 def test2(key, data):
+    """Test the 'test.2' event."""
     LOGGER.warn("Test2 successful!\n%s - %s", key, data)
     return True
 
 
 @subscribe_to("onexit")
 def stop_func(key, data):
+    """Test the 'onexit' event."""
     LOGGER.debug("I'm not doing anything productive anymore.")
     return True
