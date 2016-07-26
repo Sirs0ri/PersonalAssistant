92897cb0ad4b96606447c62a2fa13942e50646e8
Maximilian Ertl <ertl.maxi@gmail.com>
2016-07-26 22:01:28 +0200

    :new: Mediacenter-plugin

---
+++ samantha/plugins/mediacenter_plugin.py
@@ -0,0 +1,47 @@
+"""A plugin to test loading devices. It doesn't do anything."""
+
+###############################################################################
+#
+# TODO: [ ]
+#
+###############################################################################
+
+
+# standard library imports
+import logging
+
+# related third party imports
+
+# application specific imports
+# pylint: disable=import-error
+import context
+from core import subscribe_to
+from tools import eventbuilder
+from plugins.plugin import Plugin
+# pylint: enable=import-error
+
+
+__version__ = "1.0.0"
+
+# Initialize the logger
+LOGGER = logging.getLogger(__name__)
+
+PLUGIN = Plugin("Mediacenter", True, LOGGER, __file__)
+
+
+@subscribe_to("chromecast.connection_change")
+def chromecast_connection_change(key, data):
+    """React to a change of the Chromecast's connection."""
+    # Check if the Chromecast is connected to an app
+    if context.get_value("time.time_of_day") is "night":
+        if data["display_name"] in [None, "Backdrop"]:  # not connected
+            eventbuilder.Event(sender_id=PLUGIN.name,
+                               keyword="turn.off.ambient.433").trigger()
+            eventbuilder.Event(sender_id=PLUGIN.name,
+                               keyword="turn.on.led").trigger()
+            return True
+        else:  # An app is connected to the Chromecast
+            eventbuilder.Event(sender_id=PLUGIN.name,
+                               keyword="turn.on.ambient.light").trigger()
+            return True
+    return False
