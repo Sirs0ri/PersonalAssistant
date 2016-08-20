21416848d08777f786aad44c8b541a059c93a6c2
Maximilian Ertl <ertl.maxi@gmail.com>
2016-08-20 10:49:42 +0200

    :tada: comments, cleanup & Docs

--- samantha/plugins/facebook_plugin.py
+++ samantha/plugins/facebook_plugin.py
@@ -1,8 +1,12 @@
-"""A plugin to test loading plugins. It doesn't do anything."""
+"""A plugin to react to new pokes from Facebook.
+
+It hijacks an authentication-cookie which the user has to enter manually.
+Nomnomnom, Cookies!
+"""
 
 ###############################################################################
 #
-# TODO: [ ] Docs
+# TODO: [ ]
 #
 ###############################################################################
 
@@ -21,22 +25,6 @@ from samantha.plugins.plugin import Plugin
 from samantha.tools.eventbuilder import eEvent
 
 
-def _init(curl):
-    if curl is None:
-        return None, None
-    curl = curl.replace("curl ", "").replace(" --compressed", "")
-    divider = curl[0]
-    # this will be the type of quote around the items in the cURL command.
-    # Should be ', but better safe than sound, right?
-    curl = curl.replace(divider, "")  # remove all the quotes
-    headers = curl.split(" -H ")
-    url = headers.pop(0)
-    header_dict = {}
-    for h in headers:
-        name, val = h.split(": ")
-        header_dict[name] = val
-    return url, header_dict
-
 try:
     import samantha.variables_private as variables_private
     CURL = variables_private.fb_curl
@@ -46,7 +34,7 @@ except (ImportError, AttributeError):
     CURL = None
 
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
 # Initialize the logger
@@ -54,8 +42,38 @@ LOGGER = logging.getLogger(__name__)
 
 PLUGIN = Plugin("Facebook", CURL is not None, LOGGER, __file__)
 
-URL, HEADER_DICT = _init(CURL)
 
+def _parse_curl(curl):
+    """Parse a cURL command meant to be used in bash into URL and headers.
+
+    This plugin requires cookie-jacking to access and parse an authenticated
+    version of Facebook's "poke" page. Chrome allows to copy internal URL call
+    the browser makes including necessary cookies as bash commands. This
+    function allows the user to enter this command and have it parsed.
+    """
+    if curl is None:
+        return None, None
+    curl = curl.replace("curl ", "").replace(" --compressed", "")
+    divider = curl[0]
+    # This will be the type of quote around the items in the cURL command.
+    # Should always be ', but better safe than sound, right?
+    curl = curl.replace(divider, "")  # remove all the quotes
+    # The command is in the format "URL -H header1 -H header2 ..."
+    # Everything before the first appearance of -H is the URL, after each
+    # appearance of -H follows a header.
+    headers = curl.split(" -H ")
+    url = headers.pop(0)
+    header_dict = {}
+    for h in headers:
+        name, val = h.split(": ")
+        header_dict[name] = val
+    return url, header_dict
+
+
+# Parse a command formatted for bash's cURL into URL and a dict of headers.
+URL, HEADER_DICT = _parse_curl(CURL)
+
+# Initialize the HTMLParser only once to be used later.
 UNESCAPE = HTMLParser().unescape
 
 CACHE = []
@@ -63,6 +81,11 @@ CACHE = []
 
 @subscribe_to("time.schedule.min")
 def check_pokes(key, data):
+    """Parse the website https://m.facebook.com/pokes/ to access new pokes.
+
+    The result is compared to an existing cache of pokes to notify the user
+    only about new ones.
+    """
     global CACHE
 
     cache = []
@@ -73,6 +96,7 @@ def check_pokes(key, data):
         r'<article class="_55wr" id="poke_live_item_[\s\S]*?</article>',
         text)
     if matches:
+        # pokes were found on the parsed webpage.
         for match in matches:
             poke = {}
 
@@ -93,7 +117,7 @@ def check_pokes(key, data):
                 m.group("pokeurl"))
 
             if poke["name"] not in CACHE:
-                LOGGER.warn(poke["text"])
+                LOGGER.debug(poke["text"])
                 eEvent(sender_id=PLUGIN.name,
                        keyword="facebook.poked",
                        data=poke).trigger()
@@ -106,7 +130,7 @@ def check_pokes(key, data):
 
     CACHE = cache
 
-    return "Found {} poke{}, {} of them new. ({})".format(
+    return "Found {} poke{}, {} of them new. (Cache: {})".format(
         len(CACHE),
         "s" if len(CACHE) is not 1 else "",
         new_count,
@@ -115,6 +139,7 @@ def check_pokes(key, data):
 
 @subscribe_to("facebook.poke")
 def poke(key, data):
+    """Poke a person via a URL including Facebook's authentication cookie."""
     if "pokeurl" not in data:
         result = "Error: The URL is missing from the data."
     elif "headers" not in data:
